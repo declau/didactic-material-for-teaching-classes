02bb64737d0e4911d86eb30c0a099a4230b4855e
declau <declaudiano89@gmail.com>
2025-02-06 20:29:21 -0300

    string type

---
+++ src/fundamentos/TipoString.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class TipoString {
+
+	public static void main(String[] args) {
+		System.out.println("Hello people".charAt(0));
+		
+		String s = "Boa Tarde";
+		System.out.println(s.concat("!!!"));
+		System.out.println(s.toLowerCase().startsWith("Boa"));
+		System.out.println(s.length());
+		System.out.println(s.endsWith("Tarde"));
+		System.out.println(s.equals("boa tarde"));
+		System.out.println(s.equalsIgnoreCase("boa tarde"));
+
+	}
+
+}
