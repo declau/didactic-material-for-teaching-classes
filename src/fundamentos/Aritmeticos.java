29b26bc9c761c1eeb530d7bcc2a7f27e997554fb
declau <declaudiano89@gmail.com>
2025-03-02 14:06:18 -0300

    arithmetic operations

---
+++ src/fundamentos/Aritmeticos.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class Aritmeticos {
+
+	public static void main(String[] args) {
+		
+		System.out.println(2 + 3);
+		
+		var num1 = 2;
+		double num2 = 3;
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		
+
+	}
+
+}
