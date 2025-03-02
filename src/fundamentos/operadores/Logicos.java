71551f99e9faae099625e3b47a5e8c9582ae65f4
declau <declaudiano89@gmail.com>
2025-03-02 15:19:19 -0300

    adjusts

--- src/fundamentos/operadores/Logicos.java
+++ src/fundamentos/operadores/Logicos.java
@@ -12,7 +12,28 @@ public class Logicos {
 		System.out.println(condicao1 ^ condicao2);
 		System.out.println(!condicao1);
 		System.out.println(!condicao2);
-
+		
+		System.out.println("Tabela verdadde E (AND)");
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+		
+		System.out.println("\nTabela verdadde OU (OR)");
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+		
+		System.out.println("\nTabela verdadde OU Exclusivo (XOR)");
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+		
+		System.out.println("Tabela verdadde (NOT)");
+		System.out.println(!true);
+		System.out.println(!false);
 	}
 
 }
