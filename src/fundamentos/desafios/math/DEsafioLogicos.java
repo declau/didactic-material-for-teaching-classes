54df9b86ed69b14d6a411407d37213a8901ea6ca
declau <declaudiano89@gmail.com>
2025-03-03 19:08:24 -0300

    logica

---
+++ src/fundamentos/desafios/math/DEsafioLogicos.java
@@ -0,0 +1,20 @@
+package fundamentos.desafios.math;
+
+public class DEsafioLogicos {
+
+	public static void main(String[] args) {
+		
+		boolean trabalho1 = true;
+		boolean trabalho2 = true;
+		
+		boolean comprouTv50 = trabalho1 && trabalho2;
+		boolean comprouTv32 = trabalho1 ^ trabalho2;
+		
+		System.out.println("Comprou TV de 50 polegadas? " + comprouTv50);
+		System.out.println("Comprou TV de 50 polegadas? " + comprouTv50);
+		
+		
+
+	}
+
+}
