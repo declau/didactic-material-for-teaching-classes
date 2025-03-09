f0057d3b8ca66ba2c8b7706441c64d2118f21d89
declau <declaudiano89@gmail.com>
2025-03-09 19:23:06 -0300

    ternary final

--- src/fundamentos/operadores/Ternario.java
+++ src/fundamentos/operadores/Ternario.java
@@ -3,11 +3,18 @@ package fundamentos.operadores;
 public class Ternario {
 
 	public static void main(String[] args) {
-		double media = 5.6;
+		double media = 9.9;
 		String resultadoParcial = media >= 5.0 ? "em recuperação" : "Reprovado";
 		String resultadoFinal = media >= 7.0 ? "Aprovado." : resultadoParcial;
 		
 		System.out.println("O aluno está " + resultadoFinal);
+		
+		double nota = 9.9;
+		boolean bomComportamento = true;
+		boolean passouPorMedia = nota >= 7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		String resultado = temDesconto ? "Sim " : "Não";
+		System.out.printf("Tem desconto? %s", resultado);
 
 	}
 
