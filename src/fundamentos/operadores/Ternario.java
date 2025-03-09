03c380276e70f6b0bc9271b372b6f7a7baa168e6
declau <declaudiano89@gmail.com>
2025-03-09 19:16:42 -0300

    ternary adjust

--- src/fundamentos/operadores/Ternario.java
+++ src/fundamentos/operadores/Ternario.java
@@ -3,7 +3,11 @@ package fundamentos.operadores;
 public class Ternario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double media = 5.6;
+		String resultadoParcial = media >= 5.0 ? "em recuperação" : "Reprovado";
+		String resultadoFinal = media >= 7.0 ? "Aprovado." : resultadoParcial;
+		
+		System.out.println("O aluno está " + resultadoFinal);
 
 	}
 
