946dbea50788327b04f8c4313e40ede8aee0b0f7
declau <declaudiano89@gmail.com>
2025-03-10 11:45:19 -0300

    basic calculator

---
+++ src/fundamentos/desafios/math/Caculadora.java
@@ -0,0 +1,22 @@
+package fundamentos.desafios.math;
+
+public class Caculadora {
+
+	public static void main(String[] args) {
+		
+		double num1 = 2;
+		double num2 = 2;
+		
+		double soma  = (num1 + num2);
+		System.out.printf("O resultado da soma é: %f", soma);
+		double subtracao = (num1 - num2);
+		System.out.printf("\nO resultado da subtração é: %f", subtracao);
+		double multiplicacao = (num1 * num2);
+		System.out.printf("\nO resultado da multiplicação é: %f", multiplicacao);
+		double divisao = (num1 / num2);
+		System.out.printf("\nO resultado da divisão é: %f", divisao);
+				
+
+	}
+
+}
