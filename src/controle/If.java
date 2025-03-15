5910b6d1bce40b110b15c3a597d4237464b05e5b
declau <declaudiano89@gmail.com>
2025-03-15 13:54:34 -0300

    if start

--- src/controle/If.java
+++ src/controle/If.java
@@ -1,8 +1,17 @@
 package controle;
 
+import java.util.Scanner;
+
 public class If {
 
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe a média: ");
+		double nota = entrada.nextDouble();
+		
+		
+		entrada.close();
 		
 
 	}
