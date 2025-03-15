8bfb12a2595b415665ba1ea4e1260571ea064c02
declau <declaudiano89@gmail.com>
2025-03-15 14:05:27 -0300

    adjust

--- src/controle/If.java
+++ src/controle/If.java
@@ -21,7 +21,7 @@ public class If {
 		
 		if (media >= 7.0) {
 			System.out.println("Aluno aprovado!");
-		} else if (media >=5 5.0 && media <= 6.9) {
+		} else if (media >= 5.0 && media <= 6.9) {
 			System.out.println("Aluno em recuperação!!!");
 			
 		} else 
